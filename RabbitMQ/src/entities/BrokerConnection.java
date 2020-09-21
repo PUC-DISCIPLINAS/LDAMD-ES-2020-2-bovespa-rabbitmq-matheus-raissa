1b63bd8d0c65a2e46ec9d3975e1399766e481b5c
Raissa Silva <rcvsilva@sga.pucminas.br>
2020-09-21 16:13:29 -0300

    adding rabbitmq project

---
+++ RabbitMQ/src/entities/BrokerConnection.java
@@ -0,0 +1,38 @@
+package entities;
+
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+
+public class BrokerConnection extends Thread {
+
+    private String host;
+    private String topic;
+    private String message;
+
+    public BrokerConnection(String host, String topic, String message) {
+        this.host = host;
+        this.topic = topic;
+        this.message = message;
+    }
+
+    @Override
+    public void run() {
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(host);
+
+        try {
+            Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel();
+
+            channel.exchangeDeclare("BROKER", BuiltinExchangeType.TOPIC);
+
+            channel.basicPublish("BROKER", topic, null, message.getBytes("UTF-8"));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ RabbitMQ/src/entities/StockReceive.java
@@ -0,0 +1,81 @@
+package entities;
+
+import gui.*;
+import utils.*;
+
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+
+import com.rabbitmq.client.*;
+
+public class StockReceive extends Thread {
+
+    private String host;
+
+    public StockReceive(String host) {
+        this.host = host;
+    }
+
+    @Override
+    public void run() {
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(host);
+
+        try {
+            Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel();
+
+            channel.exchangeDeclare("BROKER", BuiltinExchangeType.TOPIC);
+            String queueName = channel.queueDeclare().getQueue();
+            channel.queueBind(queueName, "BROKER", "#");
+
+            JLabel openMsgL = new JLabel("Negociacoes abertas...");
+            openMsgL.setFont(new Font("Serif", Font.BOLD, 12));
+            openMsgL.setForeground(Color.MAGENTA);
+            openMsgL.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+            StockGUI.actionsP.add(openMsgL);
+            StockGUI.actionsP.revalidate();
+            StockGUI.actionsP.repaint();
+
+            DeliverCallback deliveryCallback = (consumerTag, delivery) -> {
+                String message = new String(delivery.getBody(), "UTF-8");
+                String topic = delivery.getEnvelope().getRoutingKey();
+
+                String[] splitTopic = topic.split("\\.");
+                String[] splitMessage = message.split(">|;|<");
+
+                String messageFormat;
+
+                if (splitMessage.length > 5) {
+                    messageFormat = splitTopic[0] + " " + splitTopic[1].toUpperCase() + " | data: " + splitMessage[1]
+                            + ", crr_vd: " + splitMessage[2] + ", crr_cp: " + splitMessage[3] + ", qtde: "
+                            + splitMessage[4] + ", val: " + splitMessage[5];
+                } else {
+                    messageFormat = splitTopic[0] + " " + splitTopic[1].toUpperCase() + " | qtde: " + splitMessage[1]
+                            + ", val: " + splitMessage[2] + ", crr: " + splitMessage[3];
+                }
+
+                JLabel messageL = new JLabel(messageFormat);
+                messageL.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+
+                StockGUI.actionsP.add(messageL);
+                StockGUI.actionsP.revalidate();
+                StockGUI.actionsP.repaint();
+
+                StockConnection sEmit = new StockConnection(host, topic, message);
+                sEmit.start();
+
+                OfferBook.store(host, topic, message);
+            };
+
+            channel.basicConsume(queueName, true, deliveryCallback, consumerTag -> { });
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
